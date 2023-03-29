1de43dd17195f69aaec8a939ac55020a3b4f8d75
Ashok kumar Ganji <g.ashokkumar2k22@gmail.com>
2023-03-29 18:10:54 -0500

    one change in readme file

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 My name is Ashok. 
 
-(my render server link :)[https://ashokkumar-1k4o.onrender.com]
+[my render server link :](https://ashokkumar-1k4o.onrender.com)
--- routes/computation.js
+++ routes/computation.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var ran=Math.random()*100;
+var rand1 = Math.random()*100;
+var rand2 = Math.random()*100;
 var num1=0;
 var num2=0;
 var num3=0;
@@ -10,11 +11,14 @@ var num3=0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   
-  num1=Math.imul(ran);
-  num2=Math.log(ran);
-  num3=Math.log10(ran);
+  num1=Math.imul(rand1,rand2);
+  num2=Math.log(rand1);
+  num3=Math.log10(rand1);
   
-  res.send(`imul applied to ran is: ${num1},<br>log applied to ran is: ${num2},<br>log10 applied to ran is: ${num3}`);
+  //res.send(`The random values is: ${ran},<br>imul applied to ran is: ${num1},<br>log applied to ran is: ${num2},<br>log10 applied to ran is: ${num3}`);
+  
+  
+  res.send(`The Random numbers are ${rand1} and ${rand2} <br> <br> imul applied to ${rand1},${rand2} is ${num1}  <br> <br> log applied to ${rand1},${rand2} is ${num2}  <br> <br>  log10 applied to ${rand1} is ${num3}`);
 
 
 });
